e7097a9186981796ca224206b267b7aa4687caea
manuelfc1 <manuel.fuentesc@gmail.com>
2014-08-04 00:43:02 +0200

    Express and Angular Scaffolding

---
+++ server/app.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+/*
+** Development Settings
+*/
+
+if(app.get('env')==='development') {
+    app.use(express.static(path.join(__dirname,'../client')));
+    app.use(express.static(path.join(__dirname,'../client/.tmp')));
+    app.use(express.static(path.join(__dirname,'../client/app')));
+
+    //Error handling
+    app.use(function(err,req,res,next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    });
+}
+
+/*
+ * Production settings
+ */
+
+if (app.get('env')=== 'production') {
+    app.use(express.static(path.join(__dirname,'/dist')));
+    app.use(function(err,req,res,next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    });
+}
+
+module.exports = app;
